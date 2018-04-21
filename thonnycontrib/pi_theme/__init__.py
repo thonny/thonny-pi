29dc5fd72a03bf47fabff0d2fe7da0046034b765
Aivar Annamaa <aivar.annamaa@gmail.com>
2018-04-22 02:57:36 +0300

    Misc tweaks

--- thonnycontrib/pi_theme/__init__.py
+++ thonnycontrib/pi_theme/__init__.py
@@ -1,48 +1,35 @@
 import os.path
 import re
 from thonny import get_workbench
+from thonny.ui_utils import scale
 
-def pi():
+CONFIGURATION_PATH = "~/.config/lxsession/LXDE-pi/desktop.conf"
+
+def pix():
     MAIN_BACKGROUND="#ededed"
 
-    images = {
-        "run.run_current_script.gif"    : "media-playback-start.png",
-        "run.stop.gif"                  : "process-stop.png",
-        "file.new_file.gif"             : "document-new.png",
-        "file.open_file.gif"            : "document-open.png",
-        "file.save_file.gif"            : "document-save.png",
-        "tab_close.gif"                 : 'window-close.png',
-        "tab_close_active.gif"          : "window-close-act.png",
-        "run.debug_current_script.gif"  : "debug-run.png",
-        "run.step_over.gif"             : "debug-step-over.png",
-        "run.step_into.gif"             : "debug-step-into.png",
-        "run.step_out.gif"              : "debug-step-out.png",
-        "run.run_to_cursor.gif"         : "debug-run-cursor.png",
-    }
-
-    res_dir = os.path.join(os.path.dirname(__file__), "res")
-    abs_images = {original : os.path.join(res_dir, images[original]) for original in images}
     
+    res_dir = os.path.join(os.path.dirname(__file__), "res")
     scrollbar_button_settings = {}
-    for direction in ["up", "down", "left", "right"]:
+    for direction, element_name in [
+        ("up", 'Vertical.Scrollbar.uparrow'), 
+        ("down", 'Vertical.Scrollbar.downarrow'), 
+        ("left", 'Horizontal.Scrollbar.leftarrow'),
+        ("right", 'Horizontal.Scrollbar.rightarrow')]:
         # load the image
-        element_name = "scrollbar-button-" + direction
+        img_name = "scrollbar-button-" + direction
         for suffix in ["", "-insens"]:
-            img_name = element_name + suffix
-            get_workbench().get_image(os.path.join(res_dir, img_name + ".png"), img_name)
+            get_workbench().get_image(os.path.join(res_dir, img_name + suffix + ".png"),
+                                      img_name + suffix)
         
         scrollbar_button_settings[element_name] = {
             "element create" : (
-                "image", element_name,
-                ("!disabled", element_name),
-                ("disabled", element_name + "-insens"),
+                "image", img_name,
+                ("!disabled", img_name),
+                ("disabled", img_name + "-insens"),
             )
         }
     
-    from tkinter import ttk
-    print(ttk.Style().element_names())
-    print(ttk.Style().element_options("padding"))
-    print(ttk.Style().element_options("border"))
     settings = {
         "." : {
             "configure" : {
@@ -80,24 +67,26 @@ def pi():
                                 ("selected", MAIN_BACKGROUND)],
             }
         },
-        "ButtonNotebook.Tab" : {
+        "ButtonNotebook.TNotebook.Tab" : {
             "map" : {
                 "background" : [("!selected", "#d0d0d0"), 
                                 ("selected", MAIN_BACKGROUND)],
+                "padding" : [("selected", [scale(4),scale(2),scale(4),scale(3)]),
+                             ("!selected", [scale(4),scale(2),scale(4),scale(3)])]
             }
         },
         "TScrollbar" : {
             "configure" : {
                 "gripcount" : 0,
                 "borderwidth" : 0,
-                "padding" : 1,
+                "padding" : scale(1),
                 "relief" : "solid",
                 "background" : "#9e9e9e",
                 "darkcolor" : "#d6d6d6",
                 "lightcolor" : "#d6d6d6",
                 "bordercolor" : "#d6d6d6",
                 "troughcolor" : "#d6d6d6",
-                "arrowsize" : 1,
+                "arrowsize" : scale(1),
                 "arrowcolor" : "gray",
             },
             "map" : {
@@ -106,11 +95,13 @@ def pi():
                 "lightcolor" : [],
             }
         },
+        
+        # Padding allows twaking thumb width
         "Vertical.TScrollbar" : {
             "layout" : [
                 ('Vertical.Scrollbar.trough', {'sticky': 'ns', 'children': [
-                    ('scrollbar-button-up', {'side': 'top', 'sticky': ''}), 
-                    ('scrollbar-button-down', {'side': 'bottom', 'sticky': ''}), 
+                    ('Vertical.Scrollbar.uparrow', {'side': 'top', 'sticky': ''}), 
+                    ('Vertical.Scrollbar.downarrow', {'side': 'bottom', 'sticky': ''}), 
                     ("Vertical.Scrollbar.padding", {'sticky' : 'nswe', 'children' : [
                         ('Vertical.Scrollbar.thumb', {'expand' : 1, 'sticky': 'nswe'}),
                     ]}),
@@ -121,10 +112,10 @@ def pi():
         "Horizontal.TScrollbar" : {
             "layout" : [
                 ('Horizontal.Scrollbar.trough', {'sticky': 'we', 'children': [
-                    ('scrollbar-button-left', {'side': 'left', 'sticky': ''}), 
-                    ('scrollbar-button-right', {'side': 'right', 'sticky': ''}), 
+                    ('Horizontal.Scrollbar.leftarrow', {'side': 'left', 'sticky': ''}), 
+                    ('Horizontal.Scrollbar.rightarrow', {'side': 'right', 'sticky': ''}), 
                     ("Horizontal.Scrollbar.padding", {'sticky' : 'nswe', 'children' : [
-                        ('Horizontal.Scrollbar.thumb', {'expand': '1', 'sticky': 'nswe'})
+                        ('Horizontal.Scrollbar.thumb', {'expand': 1, 'sticky': 'nswe'})
                     ]}),
                 ]})
             ],
@@ -137,6 +128,13 @@ def pi():
                 "lightcolor" : [("disabled", "#d6d6d6")],
             }
         },
+        
+        "TCombobox" : {
+            "configure" : {
+                "arrowsize" : scale(10),
+            }
+        },
+
         "Menubar" : {
             "configure" : {
                 "background" : MAIN_BACKGROUND,
@@ -151,7 +149,7 @@ def pi():
                 "relief" : "flat",
                 "borderwidth" : 1,
                 "activeborderwidth" : 0,
-                #"activebackground" : bg, 
+                #"activebackground" : bg, # updated below
                 #"activeforeground" : fg, 
             }
         },
@@ -170,50 +168,80 @@ def pi():
                 "shortcuts_in_tooltips" : False
             }
         },
-        "IMAGES" : {
-            "configure" : abs_images
-        },
     }
     
     settings.update(scrollbar_button_settings)
     
     # try to refine settings according to system configuration
-    configuration_path = os.path.expanduser ("~/.config/lxsession/LXDE-pi/desktop.conf")
-    if os.path.exists(configuration_path):
-        with open(configuration_path) as fp:
+    if os.path.exists(CONFIGURATION_PATH):
+        with open(CONFIGURATION_PATH) as fp:
             for line in fp:
-                if "sGtk/FontName" in line:
-                    fontname = re.search (r"=([^0-9]*) ([0-9]*)", line, re.M).group(1)  # @UndefinedVariable
-                    fontsize = re.search (r"=([^0-9]*) ([0-9]*)", line, re.M).group(2)  # @UndefinedVariable
-                    if re.search (r'\bBold\b', fontname):
-                        fontweight = "bold"
-                        fontname = fontname.replace (" Bold", "")
-                    else:
-                        fontweight = "normal"
-                    if re.search (r'\bItalic\b', fontname):
-                        fontslant = "italic"
-                        fontname = fontname.replace (" Italic", "")
-                    else:
-                        fontslant = "roman"
-                
-                    fontspec = {"family" : fontname, "size" : fontsize,
-                                "weight" : fontweight, "slant" : fontslant}
-                    settings["TkDefaultFont"] = {"config" : fontspec}
-                    settings["TkMenuFont"] =    {"config" : fontspec}
-                    settings["TkHeadingFont"] = {"config" : fontspec}
-                
-                elif "sGtk/ColorScheme" in line:
+                if "sGtk/ColorScheme" in line:
                     bgr = re.search (r"selected_bg_color:#([0-9a-fA-F]*)", line, re.M).group(1)  # @UndefinedVariable
                     fgr = re.search (r"selected_fg_color:#([0-9a-fA-F]*)", line, re.M).group(1)  # @UndefinedVariable
                     settings["Menu"]["configure"]["activeforeground"] = "#" + fgr[0:2] + fgr[4:6] + fgr[8:10] 
-                    settings["Menu"]["configure"]["activebackground"] = "#" + bgr[0:2] + bgr[4:6] + bgr[8:10] 
+                    settings["Menu"]["configure"]["activebackground"] = "#" + bgr[0:2] + bgr[4:6] + bgr[8:10]
     
-    
-    return settings
-    
+    return settings 
 
-
+def update_fonts():
+    if not os.path.exists(CONFIGURATION_PATH):
+        return
+    
+    with open(CONFIGURATION_PATH) as fp:
+        for line in fp:
+            if "sGtk/FontName" in line:
+                fontname = re.search(r"=([^0-9]*) ([0-9]*)", line, re.M).group(1)  # @UndefinedVariable
+                fontsize = re.search(r"=([^0-9]*) ([0-9]*)", line, re.M).group(2)  # @UndefinedVariable
+                if re.search(r'\bBold\b', fontname):
+                    fontweight = "bold"
+                    fontname = fontname.replace(" Bold", "")
+                else:
+                    fontweight = "normal"
+                if re.search(r'\bItalic\b', fontname):
+                    fontslant = "italic"
+                    fontname = fontname.replace(" Italic", "")
+                else:
+                    fontslant = "roman"
+                
+                from tkinter import font
+                for name in ["TkDefaultFont", "TkMenuFont", "TkHeadingFont"]:
+                    font.nametofont(name).configure(family=fontname, size=fontsize,
+                                                    weight=fontweight, slant=fontslant)
+                    
 def load_plugin():
-    get_workbench().add_ui_theme("Raspberry Pi", "Enhanced Clam", pi)
+    """Note that fonts and some images are set globally, 
+    ie. all themes will inherit these"""
+    update_fonts()
+    
+    # set custom images
+    images = {
+        "run-current-script"   : "media-playback-start.png",
+        "stop"                 : "process-stop.png",
+        "new-file"             : "document-new.png",
+        "open-file"            : "document-open.png",
+        "save-file"            : "document-save.png",
+        "debug-current-script" : "debug-run.png",
+        "step-over"            : "debug-step-over.png",
+        "step-into"            : "debug-step-into.png",
+        "step-out"             : "debug-step-out.png",
+        "run-to-cursor"        : "debug-run-cursor.png",
+        "tab-close"            : 'window-close.png',
+        "tab-close-active"     : "window-close-act.png",
+    }
+
+    res_dir = os.path.join(os.path.dirname(__file__), "res")
+    global_image_map = {}
+    theme_image_map = {}
+    for image in images:
+        if image in ["tab-close", "tab-close-active"]:
+            target = theme_image_map
+        else:
+            target = global_image_map
+        
+        target[image] = os.path.join(res_dir, images[image])
+    
+    get_workbench().update_image_mapping(global_image_map)
+    get_workbench().add_ui_theme("Raspberry Pi", "Enhanced Clam", pix, theme_image_map)
     get_workbench().set_default("view.ui_theme", "Raspberry Pi")
 
